a399ad96c98f6a036a301cc1f4dc6750b8e75b5b
Yao Qing <jackson@greywolf.sg>
2025-07-09 14:55:17 +0800

    added dock removed polling

--- bot.py
+++ bot.py
@@ -174,7 +174,6 @@ def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button_handler))
-    app.run_polling()
 
 if __name__ == "__main__":
     main()
